735453cb09a3495bd81025f87fb4a3b847f39915
Andrew Song <andrewsong61@gmail.com>
2020-12-12 15:27:45 -0800

    Remove unnecessary middleware

--- server/index.js
+++ server/index.js
@@ -1,15 +1,11 @@
 require('newrelic');
 /* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
-const morgan = require('morgan');
-const compression = require('compression');
 const checkout = require('../database/index.js');
 
 const app = express();
 const PORT = 3000;
 
-app.use(morgan('dev'));
-app.use(compression());
 app.use('/:id', express.static('public'));
 
 app.get('/api/checkout/:id', (req, res) => {
